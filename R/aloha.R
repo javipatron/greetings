5d55a347e027571b80aa42209ec0bb5ce76b536d
Javier Patron <jpatronmf@gmail.com>
2022-08-23 23:06:45 +0000

    changes in colors

--- R/aloha.R
+++ R/aloha.R
@@ -8,7 +8,7 @@
 #' @export
 #'
 #' @examples say_aloha("Allison")
-say_aloha <- function(name) {
+say_aloha <- function(name, color) {
 
   message <- paste("Aloha,",
                    name,
@@ -16,5 +16,13 @@ say_aloha <- function(name) {
                    emo::ji("sunny"),
                    emo::ji("ocean"))
 
-  cat(crayon::bgBlue(message))
+  if(color == "green"){
+  cat(crayon::bgGreen(message))
+  } else if (color == "blue"){
+    cat(crayon::bgBlue(message))
+  } else if (color == "white")
+    cat(crayon::bgWhite(message))
+  else {
+    print("Please enter green, blue, pink for colors" )
+  }
 }
